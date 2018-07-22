853afa4cab9e33d6bf8b2e718de6232312cada59
Ha Nguyen <nguyenttha89@gmail.com>
2018-07-22 21:28:15 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,18 @@
 //define functions here
 
+function getIt()
+{
+  $("p").on("keypress", function(key)
+  {
+    if(key.which === 13)
+    {
+      alert("Hey!")
+    }
+  })
+}
+
 $(document).ready(function(){
 
-// call functions here
+getIt()
 
 });
